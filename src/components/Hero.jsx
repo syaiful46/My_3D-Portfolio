6636520cf0f799fa4b02ca73a998efa1dae02984
syaiful46 <syaifulramadhan31121999@gmail.com>
2024-02-07 13:34:17 +0700

    Update Hero Component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
             Hi, I'm <span className="text-[#915eff]">Syaiful</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop user interface, mobile and <br className='sm:block hidden' /> web application
+            I develop mobile and <br className='sm:block hidden' /> web application
           </p>
         </div>
       </div>
